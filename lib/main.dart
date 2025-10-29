14a30adbe0c7ef52aeb5ba1aa6051b4c56f964c7
Adnan Sakr <adnansakr593-beep>
2025-10-29 15:36:29 +0300

    make home page and change theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/pages/home_page.dart';
 
 void main()
 {
@@ -12,7 +13,7 @@ class NotesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp
     (
-      home: Scaffold(),
+      home: HomePage(),
     );
   }
 }
