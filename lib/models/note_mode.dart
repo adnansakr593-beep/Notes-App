0050c1221223edd1f489bfc7d87f9b8051955b9a
Adnan Sakr <adnansakr593-beep>
2025-10-31 02:15:37 +0200

    add note class

---
+++ lib/models/note_mode.dart
@@ -0,0 +1,17 @@
+class NoteMode
+{
+  final String title;
+  final String subTitle;
+  final String date;
+  final int color;
+
+  NoteMode
+  (
+    {
+      required this.title,
+      required this.subTitle,
+      required this.date,
+      required this.color
+    }
+  );
+}
