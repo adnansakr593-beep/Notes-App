439eb174ef117b4e0e77a2d016b736c9beb622e8
Adnan Sakr <adnansakr593-beep>
2025-10-31 02:21:56 +0200

    add hive in class

--- lib/models/note_mode.dart
+++ lib/models/note_mode.dart
@@ -1,8 +1,18 @@
-class NoteMode
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 0)
+class NoteMode extends HiveObject
 {
+  @HiveType(typeId: 0)
   final String title;
+
+  @HiveType(typeId: 1)
   final String subTitle;
+
+  @HiveType(typeId: 2)
   final String date;
+  
+  @HiveType(typeId: 3)
   final int color;
 
   NoteMode
