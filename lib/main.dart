b4ea1fc4d8055e484a5688492ea665cd0ba791cb
Adnan Sakr <adnansakr593-beep>
2025-11-27 20:32:11 +0200

    provide to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/cubit/add_note_cubit.dart';
 import 'package:notes_app/helper/const.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/pages/home_page.dart';
@@ -18,13 +20,20 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) 
   {
-    return MaterialApp
-    (
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark
+    return MultiBlocProvider(
+      providers: 
+      [
+        BlocProvider(create: (context)=> AddNoteCubit() ),
+        
+      ],
+      child: MaterialApp
+      (
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
