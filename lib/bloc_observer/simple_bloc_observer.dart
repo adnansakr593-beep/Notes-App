9897e4c4b091823db208777a1ab2cce99e6c3e31
Adnan Sakr <adnansakr593-beep>
2025-11-28 16:05:35 +0200

    on change bloc observer

---
+++ lib/bloc_observer/simple_bloc_observer.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/widgets.dart';
+
+class MyBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    
+    super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    debugPrint('change$change');
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    
+  }
+} 
